219b1c5c9350eb8e24af088dfa7765364ca19afc
Davide Macario <dav.macario@gmail.com>
2026-05-21 20:00:29 +0200

    chore: prompt engineer tool description to guide LLM in choosing num_docs

--- src/notes_rag/mcp_server.py
+++ src/notes_rag/mcp_server.py
@@ -22,12 +22,21 @@ class MCPServer:
     def _register_tools(self):
         @self.mcp.tool()
         async def query_notes(query_body: QueryBody):
-            """
-            Query the user's personal notes for further context.
+            """Query the user's own notes for further context.
 
-            The query body consists of:
-            - query: string containing the summary of the information you are looking for, in less than 10 words.
-            - num_docs: number of documents you would like to retrieve.
+            Allows to tap in the knowledge base of the user to avoid making assumptions when answering the user's
+            prompts.
+            Use this tool when the user explicitly requests you look at the notes.
+
+            The query body (tool argument) consists of:
+            - query: string containing the summary of the information you are looking for. Think of it like a Google
+              search, so try to be concise and use mostly keywords.
+            - num_docs: number of documents you would like to retrieve. The higher the value, the more context you will
+              get, but keep in mind that too many documents may result in non-relevant information.
+              Typical values are:
+              - 5-10: initial search, this will be the most relevant information
+              - 10-20: if more information is needed, but not required in most cases
+              - 20+: used very rarely
             """
             nodes = await self._storage.search(query_body.query, query_body.num_docs)
             additional_context = self._build_llm_context(nodes)
